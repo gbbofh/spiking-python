a525514debbf1a019c6275b0d21aa059adf97dbd
Garrett Bates <garrett.bates@student.nmt.edu>
2019-09-26 07:14:58 -0600

    Added comments and updated readme.

--- neuron.py
+++ neuron.py
@@ -1,5 +1,14 @@
 class Neuron():
     def __init__(self, scale = 0.02, uSens = 0.2, reset = -65.0, uReset = 2.0):
+        """
+        Initializes a neuron object
+
+        Parameters:
+        (float) scale: The timescale for the recovery variable
+        (float) uSens: The sensitivity of the recovery variable
+        (float) reset: The post-spike reset value for the membrane potential
+        (float) uReset: The post-spike reset value for membrane recovery
+        """
         self.scale = scale
         self.uSens = uSens
         self.reset = reset
@@ -11,6 +20,9 @@ class Neuron():
         self.rec = self.voltage * self.uSens
 
     def update(self):
+        """
+        Updates the membrane voltage and recovery variable
+        """
 
         self.voltage += 0.5 * (0.04 * self.voltage ** 2 +
                         5 * self.voltage + 140 - self.rec + self.input)
