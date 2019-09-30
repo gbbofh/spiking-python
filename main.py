df530aa4acd6a9666d1b4c6d65c21d31719fa52a
Garrett Bates <garrett.bates@student.nmt.edu>
2019-09-30 03:46:15 -0600

    Switched simulation over to use numpy arrays to compare runtimes.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import scipy.stats as stat
 import matplotlib.pyplot as plot
 
 def main():
-    k = 50
+    k = 5
     time_max = 1000
 
     spikes = []
@@ -15,22 +15,19 @@ def main():
     net = network.Network(7 * k, 3 * k)
     t1 = datetime.datetime.now()
 
-    fig = plot.figure(figsize=(6,6))
-
     print('configured network in:', t1 - t0)
+
+    fig = plot.figure(figsize=(6,6))
 
     t0 = datetime.datetime.now()
 
     for t in range(time_max):
-        for neuron in net.get_excitatory():
-            neuron.input = 5.0 * stat.norm.rvs(size=1)[0]
-
-        for neuron in net.get_inhibitory():
-            neuron.input = 2.0 * stat.norm.rvs(size=1)[0]
+        net.input[[x for x in range(0, 7 * k)]] = 5.0 * stat.norm.rvs(size=7 * k)
+        net.input[[x for x in range(7 * k, 7 * k + 3 * k)]] = 2.0 * stat.norm.rvs(size=3 * k)
 
         tmp = net.update()
-        spikes += tmp
         for n in tmp:
+            spikes.append(n)
             times.append(t)
 
     t1 = datetime.datetime.now()
@@ -40,10 +37,10 @@ def main():
     ax1 = fig.add_subplot(111)
     ax1.plot(times, spikes, ',k')
 
-    xl, xr = ax1.get_xlim()
-    yb, yt = ax1.get_ylim()
+    #xl, xr = ax1.get_xlim()
+    #yb, yt = ax1.get_ylim()
 
-    ax1.set_aspect(abs((xr - xl) / (yb - yt))  * 1.0)
+    #ax1.set_aspect(abs((xr - xl) / (yb - yt))  * 1.0)
     ax1.axhline(color='r', y=net.numEx - 0.5, xmax=time_max)
 
     plot.show()
--- network.py
+++ network.py
@@ -1,4 +1,4 @@
-import neuron
+import numpy
 import scipy.stats as stats
 
 class Network():
@@ -17,13 +17,12 @@ class Network():
         numEx (int): The number of excitatory neurons to generate
         numIn (int): The number of inhibitory neurons to generate
         """
-        self.neurons = []
-        self.synapses = []
+
         self.numEx = numEx
         self.numIn = numIn
-        self.pSpikes = []
-
         totalNum = numEx + numIn
+
+        self.pSpikes = numpy.zeros((totalNum, 1))
 
         r = stats.uniform.rvs(size=(totalNum))
 
@@ -32,54 +31,26 @@ class Network():
         # an object oriented design in mind -- this still happens to
         # work way faster than the code that is commented out down
         # below.
-        scale = [0.02 if i < numEx else 0.02 + 0.08 * r[i]
-                 for i in range(totalNum)]
-        uSens = [0.2 if i < numEx else 0.25 - 0.05 * r[i]
-                 for i in range(totalNum)]
-        reset = [-65.0 + 15 * r[i] ** 2 if i < numEx else -65.0
-                 for i in range(totalNum)]
-        uReset = [6 - 6 * r[i] ** 2 if i < numEx else 2.0
-                 for i in range(totalNum)]
+        self.scale = numpy.array([0.02 if i < numEx else 0.02 + 0.08 * r[i]
+                 for i in range(totalNum)])
+        self.uSens = numpy.array([0.2 if i < numEx else 0.25 - 0.05 * r[i]
+                 for i in range(totalNum)])
+        self.reset = numpy.array([-65.0 + 15 * r[i] ** 2 if i < numEx else -65.0
+                 for i in range(totalNum)])
+        self.uReset = numpy.array([6 - 6 * r[i] ** 2 if i < numEx else 2.0
+                 for i in range(totalNum)])
 
-        self.neurons = [neuron.Neuron(scale[i], uSens[i], reset[i], uReset[i])
-                        for i in range(totalNum)]
-
-        self.synapses = [[0.5 * stats.uniform.rvs() for i in range(totalNum)]
-                         if j < numEx else
-                         [-stats.uniform.rvs() for i in range(totalNum)]
-                         for j in range(totalNum)]
-
-        # Old code from initial prototyping -- here for posterity
-        #for i in range(numEx):
-        #    self.neurons.append(neuron.Neuron(0.02, 0.2,
-        #                                  -65 + 15 * r[i] ** 2,
-        #                                  8 - 6 * r[i] ** 2))
-        #    self.synapses.append([])
-        #    for i in range(numEx + numIn):
-        #        self.synapses[-1].append(0.5 * stats.uniform.rvs(size=1)[0])
-
-        #    self.neurons[-1].voltage = -65
-
-        #for i in range(numIn):
-        #    self.neurons.append(neuron.Neuron(0.02 + 0.08 * r[numEx + i],
-        #                                  0.25 - 0.05 * r[numEx + i], -65, 2))
-        #    self.synapses.append([])
-        #    for i in range(numEx + numIn):
-        #        self.synapses[-1].append(-stats.uniform.rvs(size=1)[0])
-
-        #    self.neurons[-1].voltage = -65
-        #    self.neurons[-1].rec = (self.neurons[-1].uSens *
-        #                           self.neurons[-1].voltage)
+        self.synapses = numpy.array([[0.5 * stats.uniform.rvs()
+                                    for i in range(totalNum)]
+                                     if j < numEx else
+                                     [-stats.uniform.rvs()
+                                      for i in range(totalNum)]
+                                     for j in range(totalNum)])
+        self.voltage = numpy.full(totalNum, -65.0)
+        self.recovery = numpy.multiply(self.voltage, self.uSens)
+        self.input = numpy.zeros(totalNum)
 
 
-    # This can't really be optimized easily. If I flatten everything out
-    # into lists and keep them as members of Network, then I can use list
-    # comprehensions to update all of the values. This gave a speedboost of
-    # about 2x when I made the change in __init__
-    # but updating the values here isn't so simple...
-    # This is probably one of those situations where it would be more
-    # beneficial speed-wise to take a data-oriented design approach,
-    # rather than an object oriented design approach.
     def update(self):
         """
         Updates the membrane potential for all neurons in the network
@@ -88,51 +59,18 @@ class Network():
         list: The list of spikes which were generated in the previous timestep
         """
 
-        # Dead code, for now.
-        spikes = [i for i,n in enumerate(self.neurons) if n.voltage >= 30.0]
+        spikes = numpy.where(self.voltage >= 30.0)
+        self.voltage[spikes] = self.reset[spikes]
+        self.recovery[spikes] += self.uReset[spikes]
 
-        # spikes = []
+        self.input += self.synapses.dot(numpy.where(self.voltage >= 30, 1, 0))
+        self.voltage += 0.5 * (0.04 * self.voltage ** 2 +
+                       5.0 * self.voltage + 140 - self.recovery + self.input)
+        self.voltage += 0.5 * (0.04 * self.voltage ** 2 +
+                       5.0 * self.voltage + 140 - self.recovery + self.input)
+        self.recovery += self.scale * (self.voltage * self.uSens - self.recovery)
 
-        for i in spikes:
-            n = self.neurons[i]
-            n.voltage = n.reset
-            n.rec = n.rec + n.uReset
-            for j,_ in enumerate(self.synapses[i]):
-                self.neurons[j].input += self.synapses[i][j]
-
-        # This code originally did the job of the above
-        # for loop and list comprehension
-        # It is here for posterity.
-        # for i,neuron in enumerate(self.neurons):
-
-        #     if neuron.voltage >= 30.0:
-        #         spikes.append(i)
-        #         neuron.voltage = neuron.reset
-        #         neuron.rec = neuron.rec + neuron.uReset
-
-        #         for j,_ in enumerate(self.synapses[i]):
-        #             self.neurons[j].input += self.synapses[i][j]
-
-        for neuron in self.neurons:
-            neuron.update()
+        self.voltage[numpy.where(self.voltage >= 30.0)] = 30.0
 
         self.pSpikes = spikes
-        return spikes
-
-    def get_excitatory(self):
-        """
-        Retrieves only the excitatory neurons
-
-        Returns:
-        list: A list containing only the excitatory neurons
-        """
-        return self.neurons[0 : self.numEx]
-
-    def get_inhibitory(self):
-        """
-        Retrieves only the inhibitory neurons
-
-        Returns:
-        list: A list containing only the inhibitory neurons
-        """
-        return self.neurons[self.numEx : self.numEx + self.numIn]
+        return spikes[0]
